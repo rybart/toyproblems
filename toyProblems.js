a5e77c55f7881e846ad3e454dd679db9862f2f97
Ryan Bart <30918018+rybart@users.noreply.github.com>
2018-07-09 07:53:00 -0600

    fadf

--- toyProblems.js
+++ toyProblems.js
@@ -127,3 +127,19 @@ const factors = num => {
   return newArr;
 }
 
+
+/*Description:
+
+If you can't sleep, just count sheep!!
+Task:
+
+Given a number, 3 for example,
+
+return a string with a murmur: "1 sheep...2 sheep...3 sheep..."*/
+
+var countSheep = function (num){
+  let str = "";
+  for(let i = 1; i <= num; i++) { str+= `${i} sheep...`; }
+  return str;
+}
+
